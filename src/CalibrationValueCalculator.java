6135d105ff765641e9a748f153aeaa6193811bf3
bczmarko <bczmarko@intrend.hu>
2023-12-08 09:43:55 +0100

    removed unused imports

--- src/CalibrationValueCalculator.java
+++ src/CalibrationValueCalculator.java
@@ -1,13 +1,10 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public class CalibrationValueCalculator {
 
